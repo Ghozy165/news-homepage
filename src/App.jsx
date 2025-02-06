80772b6582643601087fc1c3341dc0a637808a48
Ghozy165 <85822663+Ghozy165@users.noreply.github.com>
2025-02-07 01:37:30 +0700

    feat: sidebar mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 // import { Navbar } from './components/Navbar'
 
 function App() {
+
+  const [navActive, setNavActive] = useState(false)
+
+  const handleNavToggle = () => {
+    setNavActive(!navActive)
+  }
 
   return (
     <>
@@ -13,18 +17,26 @@ function App() {
           <img src="./images/logo.svg" alt="logo" />
         </div>
         <nav className='navbar'>
-          <button className='burger-menu' aria-label='Toggle menu'>
+          <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
             <div className='burger-icon'>
               <img src="./images/icon-menu.svg" alt='icon menu' />
             </div>
           </button>
-          <ul className='nav-links'>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#new">New</a></li>
-            <li><a href="#popular">Popular</a></li>
-            <li><a href="#trending">Trending</a></li>
-            <li><a href="#categories">Categories</a></li>
-          </ul>
+          <div className={`overlay ${navActive ? 'overlay-active' : ''}`} onClick={handleNavToggle}></div>
+          <div className={`nav-links ${navActive ? 'nav-active' : ''}`}>
+            <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
+              <div className='burger-icon'>
+                <img src="./images/icon-menu-close.svg" alt='icon menu close' />
+              </div>
+            </button>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#new">New</a></li>
+              <li><a href="#popular">Popular</a></li>
+              <li><a href="#trending">Trending</a></li>
+              <li><a href="#categories">Categories</a></li>
+            </ul>
+          </div>
         </nav>
       </header>
       <div className="container">
