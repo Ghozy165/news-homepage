8427581a5b93756114b21a66624ceb6506ced8b2
Ghozy165 <85822663+Ghozy165@users.noreply.github.com>
2025-02-07 14:37:50 +0700

    feat: navbar
    seperate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 // import { Navbar } from './components/Navbar'
 
 function App() {
@@ -12,33 +13,7 @@ function App() {
 
   return (
     <>
-      <header>
-        <div className="logo">
-          <img src="./images/logo.svg" alt="logo" />
-        </div>
-        <nav className='navbar'>
-          <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
-            <div className='burger-icon'>
-              <img src="./images/icon-menu.svg" alt='icon menu' />
-            </div>
-          </button>
-          <div className={`overlay ${navActive ? 'overlay-active' : ''}`} onClick={handleNavToggle}></div>
-          <div className={`nav-links ${navActive ? 'nav-active' : ''}`}>
-            <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
-              <div className='burger-icon'>
-                <img src="./images/icon-menu-close.svg" alt='icon menu close' />
-              </div>
-            </button>
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#new">New</a></li>
-              <li><a href="#popular">Popular</a></li>
-              <li><a href="#trending">Trending</a></li>
-              <li><a href="#categories">Categories</a></li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+      <Navbar />
       <div className="container">
         <main>
           <article>
@@ -105,10 +80,6 @@ function App() {
           </div>
         </div>
       </section>
-      {/* <footer className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
-      </footer> */}
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+
 export default function Navbar(){
+
+    const [navActive, setNavActive] = useState(false)
+    
+    const handleNavToggle = () => {
+      setNavActive(!navActive)
+    }
+
     return(
-        <div>
-            <h1>Navbar</h1>
+        <header>
+        <div className="logo">
+          <img src="./images/logo.svg" alt="logo" />
         </div>
+        <nav className='navbar'>
+          <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
+            <div className='burger-icon'>
+              <img src="./images/icon-menu.svg" alt='icon menu' />
+            </div>
+          </button>
+          <div className={`overlay ${navActive ? 'overlay-active' : ''}`} onClick={handleNavToggle}></div>
+          <div className={`nav-links ${navActive ? 'nav-active' : ''}`}>
+            <button className='burger-menu' aria-label='Toggle menu' onClick={handleNavToggle}>
+              <div className='burger-icon'>
+                <img src="./images/icon-menu-close.svg" alt='icon menu close' />
+              </div>
+            </button>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#new">New</a></li>
+              <li><a href="#popular">Popular</a></li>
+              <li><a href="#trending">Trending</a></li>
+              <li><a href="#categories">Categories</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     )
 }
